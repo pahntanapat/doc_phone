f2492a0e91791f5f7a433da7db32d64426d6aedc
ธนภัทร คหบดีกนกกุล <tanapat1.10@hotmail.com>
2020-01-31 15:33:04 +0700

    add method to render HTML

--- class.task.php
+++ class.task.php
@@ -8,8 +8,8 @@ class Task extends Base{
 	const TABLE='task_type';
 	
 	public function getList(){
-		$this->sql=new PDO('','','');
-		$s = $this->sql->query('SELECT * FROM '.(self::TABLE).' ORDER BY '.(self::COL_PARENT));
+		#$this->sql=new PDO('','','');
+		$s = $this->sql->query('SELECT * FROM '.(self::TABLE).' ORDER BY '.(self::COL_PARENT).', '.(self::COL_NAME));
 		
 		return($s?$s->fetchall(PDO::FETCH_ASSOC):$s);
 	}
@@ -54,10 +54,61 @@ class LoopTask{
 		return($deep_count);
 	}
 	public static function generate($flat_assoc_array, $parent=0){
-		$root=new self();
-		$root->id=$parent;
+		$root=NULL;
+		foreach($flat_assoc_array as $k=>$v){
+			if($v[(Task::COL_ID)]==$parent){
+				$root=new self($v);
+				unset($flat_assoc_array[$k]);
+				break;
+			}
+		}
+		if(is_null($root)){
+			$root=new self();
+			$root->id=$parent;
+		}
 		$d = $root->addChild($flat_assoc_array);
 		return(array($root,$d));
+	}
+	public function __toString(){
+		// Render to HTML
+		return($this->toHTML());
+	}
+	
+	public function toHTML($edible=false, $ul=true){
+		$html=$ul?"<ul id=\"taskList\" class=\"jslists\">":'';
+		$child = count($this->detail)>0;
+		if($this->id>0){
+			$html.="<li><div class=\"tooltip\">";
+			$html.=$this->name;
+			if(count($this->detail)>0)
+				$html.="<span class=\"tooltiptext\">".($this->detail)."</span>";
+			$html.="</div>";
+			if($child||$edible)
+				$html.="<ul>";
+		}
+		
+		if($child){
+			foreach($this->child as $v){
+				$html.=$v->toHTML($edible, false);
+			}
+		}elseif($edible){
+			$html.=self::addBtnHTML($this->id);
+		}
+		if($this->id>0){
+			if($child||$edible)
+				$html.="</ul>";
+			$html.="</li>";
+			if($edible)
+				$html.=self::addBtnHTML($this->parent);
+		}
+		
+		if($ul)
+			$html.="</ul>";
+		return($html);
+	}
+	
+	protected static function addBtnHTML($parent){
+		return("<li><button data-parent=\"".($parent)."\" class=\"add\">Add</button></li>");
 	}
 }
 ?>
